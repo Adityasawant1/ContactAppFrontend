e15922db12c8de7bda89dcbb2b05f7f7b34d0105
Aditya <sawantaditya3636@gmail.com>
2026-01-20 12:06:47 +0530

    spring baseUrl updated

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL: "http://ec2-65-0-180-138.ap-south-1.compute.amazonaws.com:8080",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
